618755376537fd51144540ce04263028a459ce93
David Bernal <david.bernal.software@gmail.com>
2024-05-18 14:11:38 -0600

    se separo el componente room y tv

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import {MainComponent} from "./pages/main/main.component";
+import {GalleryComponent} from "./pages/gallery/gallery.component";
 
 export const routes: Routes = [
-  // { path: '', component: MainComponent },
-  { path: '', component: MainComponent }
+  { path: '', component: MainComponent },
+  { path: 'home', component: MainComponent },
+  { path: 'gallery', component: GalleryComponent },
   // { path: 'gallery', component: GalleryComponent },
   // { path: 'about', component: AboutComponent }
 ];
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import {dateTimestampProvider} from "rxjs/internal/scheduler/dateTimestampProvider";
+import {RouterLink, RouterLinkActive} from "@angular/router";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLinkActive,
+    RouterLink
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
